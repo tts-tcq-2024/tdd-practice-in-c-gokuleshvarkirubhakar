904f17322c7d02a384b4582e6a9a7cc026d79e52
gokuleshvar <163369258+gokuleshvarkirubhakar@users.noreply.github.com>
2024-07-19 16:17:44 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -2,45 +2,38 @@
 #include <string.h>
 #include <stdlib.h>
 
-int validate_inputs(const char* input) {
-    return ((input_string == NULL || input_string[0] == '\0'));
-}
-
-char* duplicate_input(const char *input) {
-    char *copy = (char *)malloc(strlen(input) + 1);
-    if(copy != NULL) {
-        strcpy(copy, input);
-    }
-    return copy;
-}
-
-int calculate_sum(char *token, char *delimiters)
-{   
+int calculate_sum(char *token, char *delimiters) {   
     int num = 0, sum = 0;
     while (token != NULL) {
         num = atoi(token);
         if(num <= 1000) sum += num;
         token = strtok(NULL, delimiters);
     }
-    
     return sum;
 }
 
-int add(const char *input_string) {
-    
-    if(validate_inputs(input_string)) {
-        return 0;
-    }
+void check_for_custom_delimiters(char *mod_input, char *delimiters, int *offset) {
+    if(strncmp(mod_input, "//", 2) == 0) {
+        delimiters[0] = mod_input[2]; // Replace default delimiter with custom delimiter
+        *offset = 4;
+    }    
+}
 
-    char delimiters[] = ",\n";
-    char *input_string_copy = duplicate_input(input_string);
-    if (input_string_copy == NULL) {
+int add(const char *input_string) {   
+    if((input_string == NULL || input_string[0] == '\0')) {
         return 0;
     }
     
-    char *token = strtok(input_string_copy, delimiters);
+    char delimiters[] = ",\n";
+    char *mod_input = strdup(input_string);
+    int offset = 0;
+    
+    check_for_custom_delimiters(mod_input, delimiters, &offset);
+    
+    char *token = strtok(mod_input+offset, delimiters);
     int sum = calculate_sum(token, delimiters);
 
-    free(input_string_copy);
+    free(mod_input);
+
     return sum; 
 }
