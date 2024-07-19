f0c7232b112b38dbfbb490f1d1e87f86ef0110e4
gokuleshvar <163369258+gokuleshvarkirubhakar@users.noreply.github.com>
2024-07-19 12:36:54 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,9 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int add(const char *input_string)
 {
    if (input_string == NULL || input_string[0] == '\0') {
         return 0; // The string is empty
    }
+    
+   char *input_string_copy = malloc(strlen(input_string));
+   if(input_string_copy != NULL)
+   {
+       strcpy(input_string_copy, input_string);
+   }
+
+   char delimiters[] = ",\n";
+   char *token;
+   int sum = 0;
    
-   return -1; 
+   token = strtok(input_string_copy, delimiters);
+    
+    // Walk through other tokens
+    while (token != NULL) {
+        int num = atoi(token);
+        sum += num;
+        token = strtok(NULL, delimiters);
+    }
+   
+   return sum; 
 }
