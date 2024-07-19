697e2243467e39ad58fb884a963598c158a038b9
gokuleshvar <163369258+gokuleshvarkirubhakar@users.noreply.github.com>
2024-07-19 14:08:07 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 int validate_inputs(const char* input) {
-    return ((input == NULL || input[0] == '\0'));
+    return ((input_string == NULL || input_string[0] == '\0'));
 }
 
 char* duplicate_input(const char *input) {
@@ -14,27 +14,33 @@ char* duplicate_input(const char *input) {
     return copy;
 }
 
-int calculate_sum(char *token, char *delimiters) {   
+int calculate_sum(char *token, char *delimiters)
+{   
     int num = 0, sum = 0;
     while (token != NULL) {
         num = atoi(token);
-        sum += num;
+        if(num <= 1000) sum += num;
         token = strtok(NULL, delimiters);
-    }   
+    }
+    
     return sum;
 }
 
 int add(const char *input_string) {
+    
     if(validate_inputs(input_string)) {
         return 0;
     }
+
     char delimiters[] = ",\n";
     char *input_string_copy = duplicate_input(input_string);
     if (input_string_copy == NULL) {
         return 0;
-    }   
+    }
+    
     char *token = strtok(input_string_copy, delimiters);
     int sum = calculate_sum(token, delimiters);
+
     free(input_string_copy);
     return sum; 
 }
